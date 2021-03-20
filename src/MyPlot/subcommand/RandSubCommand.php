531a17dfb049932cd24fe74c02f82ac55d7013d2
Crasher508 <you@example.com>
2021-03-20 10:37:03 +0100

    Test3

--- src/MyPlot/subcommand/RandSubCommand.php
+++ src/MyPlot/subcommand/RandSubCommand.php
@@ -4,7 +4,27 @@
 namespace MyPlot\subcommand;
 
 
-class RandSubCommand
+use MyPlot\forms\MyPlotForm;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class RandSubCommand extends SubCommand
 {
 
+    public function canUse(CommandSender $sender): bool
+    {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.auto");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(CommandSender $sender, array $args): bool
+    {
+        // TODO: Implement execute() method.
+    }
+
+    public function getForm(?Player $player = null) : ?MyPlotForm {
+        return null;
+    }
 }
