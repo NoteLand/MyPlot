a1b23479725b0d1abb31681f0d4e3f31cfe026b3
Crasher508 <beyjonathaner@gmail.com>
2022-02-17 18:37:36 +0100

    Fix tree spread on plot

--- src/MyPlot/EventListener.php
+++ src/MyPlot/EventListener.php
@@ -151,7 +151,7 @@ class EventListener implements Listener
 				 * so the leaves won't go outside the plot
 				 */
 				$block = $event->getBlock();
-				$maxLengthLeaves = ($block->getIdInfo()->getVariant() == TreeType::SPRUCE()) ? 3 : 2;
+				$maxLengthLeaves = ($block->getIdInfo()->getVariant() == TreeType::SPRUCE()->getMagicNumber()) ? 3 : 2;
 				$beginPos = $this->plugin->getPlotPosition($plot);
 				$endPos = clone $beginPos;
 				$beginPos->x += $maxLengthLeaves;
