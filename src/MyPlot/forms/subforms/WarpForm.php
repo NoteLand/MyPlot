6c3e456e655da83a2182b9963c9c5d2a778cae7a
Crasher508 <beyjonathaner@gmail.com>
2022-05-24 19:52:13 +0200

    Fix WarpForm.php

--- src/MyPlot/forms/subforms/WarpForm.php
+++ src/MyPlot/forms/subforms/WarpForm.php
@@ -12,11 +12,8 @@ use pocketmine\player\Player;
 use pocketmine\utils\TextFormat;
 
 class WarpForm extends ComplexMyPlotForm {
-	/** @var Player $player */
-	private $player;
 
 	public function __construct(Player $player) {
-	    $this->player = $player;
 		$plugin = MyPlot::getInstance();
 		parent::__construct(
 			TextFormat::BLACK.$plugin->getLanguage()->translateString("form.header", [$plugin->getLanguage()->get("warp.form")]),
@@ -35,15 +32,15 @@ class WarpForm extends ComplexMyPlotForm {
 					"2",
 					$plugin->getLanguage()->get("warp.formworld"),
 					"world",
-					$player->getPosition()->getWorld()->getFolderName()
+					$player->getWorld()->getFolderName()
 				)
 			],
 			function(Player $player, CustomFormResponse $response) use ($plugin) : void {
-				if(is_numeric($response->getString("0")) and is_numeric($response->getString("1")))
+				if(is_numeric($response->getString("0")) and is_numeric($response->getString("1")) and $plugin->isLevelLoaded($response->getString("2")))
 					$data =[
 						(int)$response->getString("0"),
 						(int)$response->getString("1"),
-						$response->getString("2") === '' ? $player->getPosition()->getWorld()->getFolderName() : $response->getString("2")
+						$response->getString("2") === '' ? $player->getWorld()->getFolderName() : $response->getString("2")
 					];
 				elseif($response->getString("0") === '' and $response->getString("1") === '') {
 					$player->sendForm(new self($player));
@@ -51,7 +48,7 @@ class WarpForm extends ComplexMyPlotForm {
 				}else
 					throw new FormValidationException("Unexpected form data returned");
 
-				$player->getServer()->dispatchCommand($player, $plugin->getLanguage()->get("command.name")." ".$plugin->getLanguage()->get("warp.name")." ".((int)$response->getString("0")).";".((int)$response->getString("1")).' "'.($response->getString("2") === '' ? $this->player->getPosition()->getWorld()->getFolderName() : $response->getString("2")).'"', true);
+				$player->getServer()->dispatchCommand($player, $plugin->getLanguage()->get("command.name")." ".$plugin->getLanguage()->get("warp.name")." $data[0];$data[1] $data[2]", true);
 			}
 		);
 	}
